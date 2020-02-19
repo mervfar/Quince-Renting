5ff899e774ca262ddb3821eaae7a5f932c79c086
mervfar <of_yildirim@hotmail.com>
2020-02-19 17:26:56 +0300

    initial commit for backend

---
+++ backend/src/main/java/com/quince/QuincePanel/domain/Car.java
@@ -0,0 +1,14 @@
+package com.quince.QuincePanel.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+
+    private String name;
+    private String transmission;
+    private String fuelType;
+    private String carClass;
+}
