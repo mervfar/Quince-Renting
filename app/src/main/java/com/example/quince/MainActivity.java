9d21d59e060d5238b98670bcd047600054b03766
Gizem Akdemir <61235839+GizemAkdemir@users.noreply.github.com>
2020-02-27 16:31:27 +0300

    open page

--- app/src/main/java/com/example/quince/MainActivity.java
+++ app/src/main/java/com/example/quince/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.quince;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Thread thread = new Thread(){
+            public void run(){
+
+                try{
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext() , karsilama.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 }
