78668f885df95b448dc391dd5e88ebf8c21a173f
Anıl Öztürk <iletisim@baykatre.com>
2020-02-28 16:48:45 +0300

    some bug fixes
    cars entity fixed
    
    Co-Authored-By: Ömer Faruk YILDIRIM <iletisim@mervfar.com>

--- backend/src/main/java/com/quince/rentingapp/controller/CarsController.java
+++ backend/src/main/java/com/quince/rentingapp/controller/CarsController.java
@@ -1,8 +1,12 @@
 package com.quince.rentingapp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/cars")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CarsController {
 
 }
--- backend/src/main/java/com/quince/rentingapp/domain/cars/Cars.java
+++ backend/src/main/java/com/quince/rentingapp/domain/cars/Cars.java
@@ -1,4 +1,17 @@
 package com.quince.rentingapp.domain.cars;
 
-public class Cars {
+import com.quince.rentingapp.domain.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Table(name = "cars")
+public class Cars extends BaseEntity {
+
+    private String name;
 }
