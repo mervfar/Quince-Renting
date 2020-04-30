b773fb7f0347f4ee7a28728e2978c10685b39ab5
mervfar <of_yildirim@hotmail.com>
2020-05-01 01:58:05 +0300

    Update RegistrationController.java

--- src/main/java/com/quince/rentingapp/controller/RegistrationController.java
+++ src/main/java/com/quince/rentingapp/controller/RegistrationController.java
@@ -5,12 +5,15 @@ import com.quince.rentingapp.domain.user.Role;
 import com.quince.rentingapp.domain.user.UserAddDTO;
 import com.quince.rentingapp.service.RegistrationService;
 import com.quince.rentingapp.domain.user.User;
+import com.quince.rentingapp.service.UploadService;
 import com.quince.rentingapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Collections;
 
 @RestController
@@ -20,11 +23,14 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
     private final UserService userService;
+    private final UploadService uploadService;
 
 
 
     @PostMapping
-    public String  createUser(@RequestBody UserAddDTO addDTO){
+    public String  createUser(
+            @RequestBody UserAddDTO addDTO,
+            @RequestParam(value = "file",required = false) MultipartFile file) throws IOException {
         if (userService.existsByEmail(addDTO.getEmail())){
             if (userService.existsByUsername(addDTO.getUsername())){
                 return "{\"result\":False,\"info\":\"There is already a user registered with the credentials provided!\"}";
@@ -36,6 +42,9 @@ public class RegistrationController {
         }
         User newUser= Utils.mapper(addDTO,User.class);
         newUser.setUserRole(Role.MEMBER);
+        if(!file.isEmpty()){
+            newUser.setImageUrl(uploadService.uploadFile(file,newUser.getUsername()));
+        }
         final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         final String encodedPassword = bCryptPasswordEncoder.encode(addDTO.getPassword());
         newUser.setPassword(encodedPassword);
