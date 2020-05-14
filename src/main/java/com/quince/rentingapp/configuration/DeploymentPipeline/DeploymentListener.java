35cddaf2cdc4eee0450689a3bf2bd34a54f2bf4f
mervfar <of_yildirim@hotmail.com>
2020-05-14 20:50:08 +0300

    Update DeploymentListener.java

--- src/main/java/com/quince/rentingapp/configuration/DeploymentPipeline/DeploymentListener.java
+++ src/main/java/com/quince/rentingapp/configuration/DeploymentPipeline/DeploymentListener.java
@@ -7,11 +7,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+import java.util.concurrent.Executors;
 
 
 @RestController
@@ -36,9 +38,30 @@ public class DeploymentListener {
         return new JSONObject(content);
     }
     private void runPipeLine() throws IOException {
-        ProcessBuilder builder = new ProcessBuilder();
-        builder.command("sh", "-c", "./deneme.sh");
-        Process process = builder.start();
-        System.out.println(process);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        // Run a shell script
+        processBuilder.command("/root/deploy.sh");
+        try {
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                //abnormal...
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
