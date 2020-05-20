ab7914a460dbb24c3d7310859585eb918bf4ed78
mervfar <of_yildirim@hotmail.com>
2020-05-20 18:12:42 +0300

    Car View DTO düzeltildi.

--- src/main/java/com/quince/rentingapp/domain/car/CarViewDTO.java
+++ src/main/java/com/quince/rentingapp/domain/car/CarViewDTO.java
@@ -4,19 +4,14 @@ import lombok.Data;
 
 @Data
 public class CarViewDTO {
-    private Long id;
     private String name;
-    private Double engineSize;
-    private String colorCode;
     private String brand;
     private String series;
     private String price;
-    private double year;
     private String fuel;
     private String carBody;
     private String gear;
     private String imageUrl;
     private String gltfUrl;
-    private boolean available;
 
 }
